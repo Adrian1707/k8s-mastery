70115939429fd934cd845f0d5dd5825ff993e4e9
Adrian1707 <adrian.booth17@gmail.com>
2018-11-10 19:16:15 +0000

    use the correct target port and image name

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://192.168.99.100:32430/sentiment', {
+        fetch('http://192.168.99.100:32449/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
